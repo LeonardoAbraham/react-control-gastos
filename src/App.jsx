f9989c47e4436f2f3afddcde1bf2c161383bf168
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-08-29 12:43:16 -0600

    Validando los Gastos

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,19 @@ function App() {
     const [modal, setModal] = useState(false);
     const [animarModal, setAnimarModal] = useState(false)
 
+    const [gastos, setGastos] = useState([])
+
     const handleNuevoGasto = () => {
         setModal(true);
         
         setTimeout(() => {
             setAnimarModal(true);
         }, 500);
+    }
+
+    const guardarGasto = gasto => {
+        console.log(gasto)
+        
     }
 
     return (
@@ -43,6 +50,7 @@ function App() {
                             setModal={setModal}
                             animarModal={animarModal}
                             setAnimarModal={setAnimarModal}
+                            guardarGasto={guardarGasto}
                         />}
         </div>
     )
