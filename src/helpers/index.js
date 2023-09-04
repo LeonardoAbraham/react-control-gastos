1b7b54059310d4a41e8bea14d8631aacc7eee283
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-09-03 19:57:34 -0600

    Formateando las fechas

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,3 +5,14 @@ export const generarId = () => {
     const fecha = Date.now().toString(36)
     return random + fecha
 }
+
+
+export const formatearFecha = fecha => {
+    const fechaNueva = new Date(fecha);
+    let opciones = {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+    }
+    return fechaNueva.toLocaleDateString('es-ES', opciones)
+}
