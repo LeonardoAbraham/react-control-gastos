a9c9cbafac3d3912f9919c64888be50e167d0f4a
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-09-05 17:13:05 -0600

    Colocando Gastos Filtrados en el State

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
     const [gastoEditar, setGastoEditar] = useState({})
 
     const [filtro, setFiltro] = useState('')
+    const [gastosFiltrados, setGastosFiltrados] = useState([])
 
     useEffect(() => {
         if(Object.keys(gastoEditar).length > 0){
@@ -43,7 +44,9 @@ function App() {
 
     useEffect(() => {
         if(filtro) {
-            console.log('filtrando... ', filtro)
+            const gastosFiltrados = gastos.filter(gasto => gasto.categoria === filtro)
+
+            setGastosFiltrados(gastosFiltrados)
         }
     },[filtro])
 
