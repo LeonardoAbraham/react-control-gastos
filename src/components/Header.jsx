639376af1c5b8267c5058aa9fb7eb569ba965cbd
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-09-06 09:28:54 -0600

    Añadiendo un botón para reiniciar la app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import ControlPresupuesto from './ControlPresupuesto'
 
 const Header = ({
     gastos,
+    setGastos,
     presupuesto, 
     setPresupuesto, 
     isValidPresupuesto, 
@@ -18,7 +19,10 @@ const Header = ({
                 (
                     <ControlPresupuesto 
                         gastos={gastos}
+                        setGastos={setGastos}
                         presupuesto={presupuesto}
+                        setPresupuesto={setPresupuesto}
+                        setValidPresupuesto={setValidPresupuesto}
                     />
                 ) 
                 :
